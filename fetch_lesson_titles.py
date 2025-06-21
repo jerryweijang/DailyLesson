1051b3ef9f05ff894fb34c13c2662839e6c8eff0
jerryweijang <jerryweijang@hotmail.com>
2025-06-21 19:43:59 +0800

    refactor: 重構抓取標題的邏輯，將過濾功能整合至 fetch_titles 函數

--- fetch_lesson_titles.py
+++ fetch_lesson_titles.py
@@ -4,28 +4,76 @@ from bs4 import BeautifulSoup
 import time
 import re
 
-# 設定 Chrome options
-options = Options()
-options.add_argument('--headless')
-options.add_argument('--no-sandbox')
-options.add_argument('--disable-dev-shm-usage')
+def fetch_titles(subject_name, url, title_selector, title_filter=None):
+    options = Options()
+    options.add_argument('--headless')
+    options.add_argument('--no-sandbox')
+    options.add_argument('--disable-dev-shm-usage')
+    driver = webdriver.Chrome(options=options)
+    try:
+        driver.get(url)
+        time.sleep(5)
+        soup = BeautifulSoup(driver.page_source, 'html.parser')
+        titles = soup.select(title_selector)
+        for title in titles:
+            text = title.get_text(strip=True)
+            if title_filter is None or title_filter(text):
+                print(f'{subject_name}\t{text}')
+    finally:
+        driver.quit()
 
-# 直接由 selenium-manager 取得 ChromeDriver
-driver = webdriver.Chrome(options=options)
+def filter_nature(text):
+    # 自然: 【數字-數字】
+    return re.match(r'【\d+-\d+】', text)
 
-try:
-    url = 'https://www.learnmode.net/course/638520/content'
-    driver.get(url)
-    time.sleep(5)  # 等待 JS 載入
+def filter_chinese(text):
+    # 國文: 只抓含【】符號的項目，如【第一課】 聲音鐘
+    return re.search(r'【[^】]+】', text)
 
-    soup = BeautifulSoup(driver.page_source, 'html.parser')
-    # 抓出所有章節名稱 <h3 class="chapter-name">
-    titles = soup.select('h3.chapter-name')
+def filter_history(text):
+    # 歷史: 只抓「主題X」或「單元X」開頭
+    return re.match(r'【\d+-\d+】', text)
 
-    # 只保留符合【數字-數字】格式的章節名稱
-    for title in titles:
-        text = title.get_text(strip=True)
-        if re.match(r'【\d+-\d+】', text):
-            print(text)
-finally:
-    driver.quit()
+def filter_geography(text):
+    # 地理: 只抓「主題X」或「單元X」開頭
+     return re.match(r'【\d+-\d+】', text)
+
+def filter_civics(text):
+    # 公民: 只抓「主題X」或「單元X」開頭
+     return re.match(r'【\d+-\d+】', text)
+
+subjects = [
+    {
+        'name': '自然',
+        'url': 'https://www.learnmode.net/course/638520/content',
+        'selector': 'h3.chapter-name',
+        'filter': filter_nature
+    },
+    {
+        'name': '國文',
+        'url': 'https://www.learnmode.net/course/638508/content',
+        'selector': 'h3.chapter-name',
+        'filter': filter_chinese
+    },
+    {
+        'name': '歷史',
+        'url': 'https://www.learnmode.net/course/638740/content',
+        'selector': 'h3.chapter-name',
+        'filter': filter_history
+    },
+    {
+        'name': '地理',
+        'url': 'https://www.learnmode.net/course/638739/content',
+        'selector': 'h3.chapter-name',
+        'filter': filter_geography
+    },
+    {
+        'name': '公民',
+        'url': 'https://www.learnmode.net/course/638741/content',
+        'selector': 'h3.chapter-name',
+        'filter': filter_civics
+    },
+]
+
+for subj in subjects:
+    fetch_titles(subj['name'], subj['url'], subj['selector'], subj['filter'])
